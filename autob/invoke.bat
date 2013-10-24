0643139ed2a8c57bb3a7821c6620d13d789c3ede
martin <martin@proenza.grm.desoft.cu>
2013-10-24 09:15:43 -0400

    simplifying use

--- autob/invoke.bat
+++ autob/invoke.bat
@@ -8,14 +8,14 @@ set script_dir=%script_dir:##=%
 set ANT_HOME=%script_dir%/3p/ant
 set PATH=%PATH%;%ANT_HOME%/bin
 
-set category=%1
-set task=%2
-
 set ant_lib_extras=%script_dir%/3p/ant/lib/extras
 
-cd ..
+set category=%1
+set target=do.%category%
 
+cd ..
 rem Add the following in the ant invocation to send output to $script_dir/temp/build_log.xml file:
 rem -logger org.apache.tools.ant.XmlLogger -logfile "%script_dir%/temp/build_log.xml"
-ant -quiet -lib "%ant_lib_extras%"  -buildfile "tasks/%category%/%task%/build.xml" do.with.dependencies
+ant -quiet -lib "%ant_lib_extras%"  -buildfile tasks/dependencies.xml %target%
+
 pause
